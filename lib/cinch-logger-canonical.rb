e690277df1b9def71c0c2aa31494fab34ad75640
Brian Haberer <bhaberer@gmail.com>
2013-05-19 00:12:50 -0700

    first commit

---
+++ lib/cinch-logger-canonical.rb
@@ -0,0 +1,9 @@
+require "cinch-logger-canonical/version"
+
+module Cinch
+  module Logger
+    module Canonical
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/cinch-logger-canonical/version.rb
@@ -0,0 +1,7 @@
+module Cinch
+  module Logger
+    module Canonical
+      VERSION = "0.0.1"
+    end
+  end
+end
